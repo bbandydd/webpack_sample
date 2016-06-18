7e4a800835b1573c4cc6e589420fa52683c06a48
Andy <bbandydd@gmail.com>
2016-06-18 23:12:07 +0800

    webpack sample for react + es6

---
+++ app/src/components/App.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import '../../assets/styles/compassStyle';
+
+import React from 'react';
+
+export default class App extends React.Component{
+    constructor(){
+        super();
+    }
+
+    componentWillMount(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    render(){
+
+        return (
+            <div>
+                App
+            </div>
+        )
+    }
+
+}
+
